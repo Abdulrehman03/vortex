b140d697d0ebba65f62f8c2303b2731f88f1b6f7
Abdul Rehman <abdulrehman@Abduls-MacBook-Pro.local>
2022-12-14 12:16:39 +0500

    changes added

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <>
       <div className="mainBody">
-        <div>
+        <div className='content'>
           <div>
             <img src={start} width={100} className='firstPic  animate__fadeInLeft animate__animated animate__delay-2s' alt='' />
             <img src={logo} className="App-logo" alt="logo" />
@@ -39,13 +39,25 @@ function App() {
 
           </div>
           <div>
-
+         {state &&  <p className='descText animate__fadeIn  animate__animated'>
+          
+          {state ===1 && `Haben Sie alte und unhandliche Papierpläne und müssen diese den Wohninteressenten präsentieren? Oder stehen Umbauten, Renovationen oder Sanierungen an und dem Architekt fehlt die Grundlage für eine saubere Abwicklung? 
+Wir digitalisieren so, dass eine problemlose Weiterbearbeitung in Ihrem CAD möglich ist. Am Schluss erhalten Sie von uns allen Plänen (als Fotodatei und) als DWG – Datei.`}
+          {state ===2 && `3D-Visualisierungen haben sich nicht nur in der Produktentwicklung, sondern vor allem in der Architektur und im Immobilienverkauf durchgesetzt. Betrachter haben so die Chance, sich ein authentisches Bild vom jeweiligen Objekt zu machen. 
+Nicht zu verkennen ist nämlich die Tatsache, dass nicht jeder Mensch mit der Fähigkeit zum räumlichen Sehen ausgestattet ist. Fügen Sie in Ihre Darstellen die dritte Dimension ein, erleichtern Sie das Verständnis und vergrößern Ihre Chancen. 
+Ein Blick auf Immobilienportale zeigt, dass zumindest eine räumliche Ansicht der jeweiligen Wohnung oder des Hauses längst zum Standard gehört.
+`}
+          {state ===3 && `Genau wie bei Visualisierungen geht es auch bei Architekturmodellen darum, Ihrem Kunden ein Gefühl für sein Objekt zu vermitteln. 
+Nicht jeder kann einen Bauplan lesen oder einen Situationsplan richtig interpretieren. Das 3D-Modell lässt sich anfassen, drehen und bewegen und hilft dem Kunden, sich sein Objekt besser vorzustellen. 
+3D Druck eignet sich ideal für Architekturprojekte: Modelle lassen sich schnell und kostengünstig für Präsentationen oder Planungen herstellen. 
+Neben Modellen von Gebäuden oder sogar detaillierten Räumen sind auch großflächige Landschafts- und Geländemodelle sowie 3D Reliefe häufig von Architekten gedruckte Objekte. 
+`}
+          
+           </p>}
           </div>
          
         </div>
- {state&&  <p className='descText animate__fadeIn  animate__animated'>Lorem Ipsum is simply dummy text of
-           the printing and typesetting industry. Lorem Ipsum has been the industry's
-            standard dummy text ever since the 1500s,</p>}
+
         <a className='footertext' href="mailto:info@3dvortex.ch">info@3dvortex.ch</a>
       </div>
       <div>
